94ab3340a8a43562ee0a97644f0c947e68ddf545
Mert Kahramantürk <kahramanturkk@gmail.com>
2025-05-08 15:03:47 +0300

    rollup-conf

---
+++ rollup.config.mjs
@@ -0,0 +1,43 @@
+import url from '@rollup/plugin-url';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json' assert { type: 'json' };
+import postcss from 'rollup-plugin-postcss';
+import postcssImport from 'postcss-import';
+import cssnano from 'cssnano';
+
+export default {
+  input: 'src/index.js',
+  output:
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: false,
+      exports: 'named'
+    },
+  external: [...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    url({
+      include: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.gif'],
+			limit: 0,
+    }),
+    postcss({
+      include: ['**/*.css'],
+      extract: true,
+      inject: true,
+      modules: false,
+			plugins: [
+        postcssImport(),
+				cssnano()
+      ],
+    }),
+    resolve({ extensions: ['.js', '.jsx'] }),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react']
+    }),
+  ]
+};
